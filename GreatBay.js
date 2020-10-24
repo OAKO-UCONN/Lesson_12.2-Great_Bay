8efa796cd628d8978a51a391eaa7074c00e83a91
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-10-23 20:39:00 -0400

    Added another connection var for Heroku.

--- GreatBay.js
+++ GreatBay.js
@@ -8,10 +8,27 @@ import inquirer from 'inquirer';
 const mysql = require('mysql');
 const inquirer = require('inquirer')
 
-//Create the connection
+//Create the connection on localhost
+var connection = mysql.createConnection({
+    host:"localhost",
+    port: 3306,
+    user:"root",
+    password:"T)UW%#T_$()qwr-a)jsrtg0-",
+    database:"greatbay_db"
+})
+
+//Create the connection on Heroku. (Coming soon.)
+/*
 var connection = mysql.createConnection({
     host:"localhost",
     port: 3306,
     user:"root",
     password:"T)UW%#T_$()qwr-a)jsrtg0-"
+    database:"greatbay_db"
+})
+*/
+
+//Console log the connection
+connection.connect(function(err){
+    console.log("Connected as id")
 })
